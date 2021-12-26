e6a59d16b91fc0991b0f81398aa6cdd6585e08a7
34636 <ibaifranco@gmail.com>
2021-12-26 18:27:35 +0100

    Firestore DBa gehituta eta CRUD froga

--- app/src/main/java/com/example/androidgral/HomeActivity.kt
+++ app/src/main/java/com/example/androidgral/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidgral
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_home.*
 
 enum class ProviderType{
@@ -10,6 +11,9 @@ enum class ProviderType{
 }
 
 class HomeActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -31,6 +35,21 @@ class HomeActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
-
+        saveButton.setOnClickListener{
+            db.collection("erabiltzaileak").document(email).set(
+                hashMapOf("provider" to provider,
+                "klub" to klubTextView.text.toString(),
+                "sarea" to sareTextView.text.toString())
+            )
+        }
+        getButton.setOnClickListener{
+            db.collection("erabiltzaileak").document(email).get().addOnSuccessListener {
+                klubTextView.setText(it.get("klub") as String?)
+                sareTextView.setText(it.get("sarea") as String?)
+            }
+        }
+        deleteButton.setOnClickListener{
+            db.collection("erabiltzaileak").document(email).delete()
+        }
     }
 }
