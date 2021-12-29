e1744c024657c213590201dba08e8eca74792530
34636 <ibaifranco@gmail.com>
2021-12-29 18:14:56 +0100

    Irudia kargatzen da mugikorretik ImageView-an

--- app/src/main/java/com/example/androidgral/NewDrillActivity.kt
+++ app/src/main/java/com/example/androidgral/NewDrillActivity.kt
@@ -1,5 +1,7 @@
 package com.example.androidgral
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthActionCodeException
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_drills.*
 import kotlinx.android.synthetic.main.activity_new_drill.*
@@ -23,16 +26,19 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso
 import java.util.HashMap
 
 class NewDrillActivity : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
-    private val postImageUri: Uri? = null
-    private val storageReference: StorageReference? = null
-    private val auth = FirebaseAuth.getInstance()
 
-
+    private var PICK_IMAGE_REQUEST = 12
+    private var imagePath: Uri?=null
+    private var firebaseStorage: FirebaseStorage?=null
+    private var storageReference: StorageReference?=null
+    private var firebaseAuth: FirebaseAuth?=null
+    private var prg: ProgressDialog?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +49,41 @@ class NewDrillActivity : AppCompatActivity() {
         val izena = bundle?.getString("izena")
         textViewId.text = izena
 
+        //irudia
+        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseStorage = FirebaseStorage.getInstance()
+        storageReference = FirebaseStorage.getInstance().reference
+
+        imageViewId.setOnClickListener {
+            fileChooser()
+        }
+
+    }
+
+    private fun fileChooser() {
+        val intent = Intent()
+        intent.type="image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent,PICK_IMAGE_REQUEST)
+    }
+
+    private fun sendData(){
+        val db = FirebaseFirestore.getInstance()
+
+        val imageRef: StorageReference = storageReference!!.child(firebaseAuth!!.uid!!).child("image").child("Drill Pic")
+        val uploadImage:UploadTask = imageRef.putFile(imagePath!!)
+        uploadImage.addOnFailureListener{
+            Toast.makeText(this,"Errore bat jazo da", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==PICK_IMAGE_REQUEST && resultCode==Activity.RESULT_OK && data!=null && data.data!=null){
+            imagePath = data.data
+            Picasso.get().load(imagePath).into(imageViewId)
+            sendData()
+        }
     }
 }
 
